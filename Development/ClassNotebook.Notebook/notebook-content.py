b53a70761d55ad18567425289dc8a6d8a321fbed
GVenkatMSFabric <Venkateswarlu@vgad222.onmicrosoft.com>
2024-12-23 15:05:11 +0530

    Added all these for Project Purpose.

--- Development/ClassNotebook.Notebook/notebook-content.py
+++ Development/ClassNotebook.Notebook/notebook-content.py
@@ -13,17 +13,18 @@
 # CELL ********************
 
 import pandas as pd
-# This is for Data Frame Creation
+
 EmployeeInfo = [
      [101,"Venkat",4545,"Chennai","MS Fabric"],
      [102,"Ramesh",232,"Bangalore","DAX"],
      [103,"Suresh",676,"Chennai","DAX"],
      [104,"Koti",343,"Hyderabad","MS Fabric"],
+     [105,"Ramu",898,"Chennai","MS Fabric"],
+     [106,"Somu",454,"Bangalore","DAX"],
      [107,"Pavani",676,"Chennai","MS Fabric"],
      [108,"Jhansi",454,"Bangalore","DAX"]
 ]
 ColumnData = ['EmpId','EmpName','EmpSalary','EmpLocation','EmpCourse']
-# We are creating one Python Data Frame
 df = pd.DataFrame(data = EmployeeInfo,columns=ColumnData)
 display(df)
 
