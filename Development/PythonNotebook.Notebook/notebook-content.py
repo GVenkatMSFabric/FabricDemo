1a00c3739cee86e0213642f42328156ae7cf5514
GVenkatMSFabric <Venkateswarlu@vgad222.onmicrosoft.com>
2024-12-23 14:45:14 +0530

    We have created one Python Notebook

---
+++ Development/PythonNotebook.Notebook/notebook-content.py
@@ -0,0 +1,36 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "jupyter",
+# META     "jupyter_kernel_name": "python3.11"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+import pandas as pd
+
+EmployeeInfo = [
+     [101,"Venkat",4545,"Chennai","MS Fabric"],
+     [102,"Ramesh",232,"Bangalore","DAX"],
+     [103,"Suresh",676,"Chennai","DAX"],
+     [104,"Koti",343,"Hyderabad","MS Fabric"],
+     [105,"Ramu",898,"Chennai","MS Fabric"],
+     [106,"Somu",454,"Bangalore","DAX"],
+     [107,"Pavani",676,"Chennai","MS Fabric"],
+     [108,"Jhansi",454,"Bangalore","DAX"]
+]
+ColumnData = ['EmpId','EmpName','EmpSalary','EmpLocation','EmpCourse']
+df = pd.DataFrame(data = EmployeeInfo,columns=ColumnData)
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "jupyter_python"
+# META }
