d00c7e14978d7d7c4274cfcdf4b78892aabc7e83
MSVenkatFabric <Venkateswarlu@vgad222.onmicrosoft.com>
2024-12-24 11:37:22 +0530

    Done Some Changes required.

---
+++ Development/TempWarehouseData.Warehouse/dbo/Tables/Dept.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[Dept] (
+
+	[EmpId] int NULL, 
+	[EmpName] varchar(100) NULL, 
+	[EmpSalary] int NULL
+);
+
---
+++ Development/TempWarehouseData.Warehouse/dbo/Tables/SalesInformatonData.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [dbo].[SalesInformatonData] (
+
+	[EmpId] int NULL, 
+	[EmpName] varchar(100) NULL, 
+	[EmpSalary] int NULL
+);
+
